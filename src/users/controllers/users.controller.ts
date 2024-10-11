52e4f1719bd7701d891a8af03d56da1a2be7acb8
FrankiNarvaez <frannki03@gmail.com>
2024-10-11 02:09:48 -0500

    Added controlers

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { UsersService } from '../services/users.service';
+import { UserDto } from '../dto/user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post('register')
+  public async registerUser(@Body() body: UserDto) {
+    return await this.usersService.createUser(body);
+  }
+
+  @Get('all')
+  public async getAllUsers() {
+    return await this.usersService.getUsers();
+  }
+  @Get(':id')
+  public async getUserById(@Param('id') id: string) {
+    return await this.usersService.getUserById(id);
+  }
+  @Patch(':id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto,
+  ) {
+    return await this.usersService.updateUser(id, body);
+  }
+  @Post(':id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.usersService.deleteUser(id);
+  }
+}
