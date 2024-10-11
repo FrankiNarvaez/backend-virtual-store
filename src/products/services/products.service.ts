6dbeceee9f36ccc3c9cd1cf5fa4c4615842b9744
FrankiNarvaez <frannki03@gmail.com>
2024-10-10 23:36:20 -0500

    added service and created dto

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductsEntity } from '../entities/products.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { ProductDto } from '../dto/product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(ProductsEntity)
+    private readonly productsRepository: Repository<ProductsEntity>,
+  ) {}
+
+  public async createProduct(body: ProductDto): Promise<ProductsEntity> {
+    try {
+      return await this.productsRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getProducts(): Promise<ProductsEntity[]> {
+    try {
+      return await this.productsRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getProductById(id: string): Promise<ProductsEntity> {
+    try {
+      return await this.productsRepository
+        .createQueryBuilder('products')
+        .where({ id })
+        .getOne();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async updateProduct(
+    id: string,
+    body: UpdateProductDto,
+  ): Promise<UpdateResult | undefined> {
+    try {
+      const product: UpdateResult = await this.productsRepository.update(
+        id,
+        body,
+      );
+      if (product.affected === 0) {
+        return undefined;
+      }
+      return product;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async deleteProduct(id: string): Promise<DeleteResult | undefined> {
+    try {
+      const product: DeleteResult = await this.productsRepository.delete(id);
+      if (product.affected === 0) {
+        return undefined;
+      }
+      return product;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
