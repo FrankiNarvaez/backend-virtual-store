ce942e8b817c6d159aa0b5e26256f69ef38a1eaa
FrankiNarvaez <frannki03@gmail.com>
2024-10-11 03:02:04 -0500

    Added end points

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductsService } from '../services/products.service';
+import { ProductDto } from '../dto/product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly prodcutsService: ProductsService) {}
+
+  @Post('create')
+  public async createProduct(@Body() body: ProductDto) {
+    return this.prodcutsService.createProduct(body);
+  }
+  @Get('all')
+  public async getProducts() {
+    return this.prodcutsService.getProducts();
+  }
+  @Get(':id')
+  public async getProductById(@Param('id') id: string) {
+    return this.prodcutsService.getProductById(id);
+  }
+  @Patch(':id')
+  public async updateProduct(
+    @Param('id') id: string,
+    @Body() body: UpdateProductDto,
+  ) {
+    return this.prodcutsService.updateProduct(id, body);
+  }
+  @Delete(':id')
+  public async deleteProduct(@Param('id') id: string) {
+    return this.prodcutsService.deleteProduct(id);
+  }
+}
