2ea81fec20a6c2cdeaec6ca1a32b2ee4695e305b
FrankiNarvaez <frannki03@gmail.com>
2024-10-11 19:33:28 -0500

    Added goblal iterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+
   app.useGlobalPipes(
     new ValidationPipe({
       transformOptions: {
@@ -14,9 +15,14 @@ async function bootstrap() {
       },
     }),
   );
+
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
   const configService = app.get(ConfigService);
   app.enableCors();
   app.setGlobalPrefix('api');
+
   await app.listen(configService.get('PORT'));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
