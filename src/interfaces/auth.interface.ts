15f83c4acc5b4d21e385cf39edf9fe2a3e0adaca
FrankiNarvaez <frannki03@gmail.com>
2024-10-11 19:31:30 -0500

    Created interfaces to auth

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -1,7 +1,25 @@
 import { ROLES } from '../constants/roles.constants';
+import { UsersEntity } from '../users/entities/users.entity';
 
 export interface PayloadToken {
   role: ROLES;
   sub: string;
 }
 
+export interface AuthTokenResult {
+  role: string;
+  sub: string;
+  iat: number;
+  exp: number;
+}
+
+export interface UseToken {
+  role: string;
+  sub: string;
+  isExpired: boolean;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  user: UsersEntity;
+}
